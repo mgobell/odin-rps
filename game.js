342802b4009a2cf13e8c6e718deff26e1f63fa62
mikeg <rangersilme@gmail.com>
2023-09-19 14:28:59 -0400

    Implement SingleRound() and DetermineWinner()

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
-console.log("Hello, World!");
-console.log(GetComputerChoice());
-console.log(GetComputerChoice());
-console.log(GetPlayerChoice());
+let round = SingleRound();
+if (round === "Tie") console.log("It's a tie!");
+else if (round === 1) console.log("You win!");
+else if (round === 2) console.log("Computer wins!");
 
 function GetComputerChoice() {
     return ParseChoice(Math.floor(Math.random() * 3) + 1);
@@ -25,6 +25,46 @@ function ParseChoice(choice) {
             break;
         case 3:
             return "Scissors"
+            break;
+    }
+}
+
+function SingleRound() {
+    let computerChoice = GetComputerChoice();
+    console.log(computerChoice);
+    let playerChoice = GetPlayerChoice();
+    return DetermineWinner(playerChoice, computerChoice);
+}
+
+function DetermineWinner(player1, player2) {
+    if (player1 === player2) return "Tie";
+    switch(player1) {
+        case "Rock":
+            if (player2 === "Paper") {
+                console.log("Paper beats rock!"); 
+                return 2;
+            } else {
+                console.log("Rock beats Scissors!");
+                return 1;
+            }
+            break;
+        case "Paper":
+            if (player2 === "Scissors") {
+                console.log("Scissors beats Paper!");
+                return 2;
+            } else {
+                console.log("Paper beats Rock!");
+                return 1;
+            }
+            break;
+        case "Scissors":
+            if (player2 === "Rock") {
+                console.log("Rock beats Scissors!");
+                return 2;
+            } else {
+                console.log("Scissors beats Paper!");
+                return 1;
+            }
             break;
     }
 }
