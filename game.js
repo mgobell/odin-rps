f650a0ec48bfd02352672a3697cba315df7e5f49
mikeg <rangersilme@gmail.com>
2023-09-19 14:12:04 -0400

    Implement GetPlayerChoice() and ParseChoice()

--- game.js
+++ game.js
@@ -1,17 +1,29 @@
 console.log("Hello, World!");
 console.log(GetComputerChoice());
 console.log(GetComputerChoice());
+console.log(GetPlayerChoice());
 
 function GetComputerChoice() {
-    randInt = Math.floor(Math.random() * 3);
-    switch(randInt) {
-        case 0:
+    return ParseChoice(Math.floor(Math.random() * 3) + 1);
+}
+
+function GetPlayerChoice() {
+    let choice = 0;
+    while (choice < 1 || choice > 3 || Number.isNaN(choice)) {
+    choice = parseInt(prompt("Please choose!\n1: Rock\n2: Paper\n3: Scissors"));
+    }
+    return ParseChoice(choice);
+}
+
+function ParseChoice(choice) {
+    switch(choice) {
+        case 1:
             return "Rock";
             break;
-        case 1:
+        case 2:
             return "Paper";
             break;
-        case 2:
+        case 3:
             return "Scissors"
             break;
     }
