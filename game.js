c90e34f6b3e8b4189d287b9ede425abee97cc617
mikeg <rangersilme@gmail.com>
2023-09-19 13:48:39 -0400

    Implement GetComputerChoice()
    Function returns rock, paper, or scissors, based on a random integer
    chosen using Math.floor and Math.random.

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
 console.log("Hello, World!");
+console.log(GetComputerChoice());
+console.log(GetComputerChoice());
+
+function GetComputerChoice() {
+    randInt = Math.floor(Math.random() * 3);
+    switch(randInt) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors"
+            break;
+    }
+}
