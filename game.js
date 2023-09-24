69d88295e5836648eeae34f5705aa89e417499e8
mikeg <rangersilme@gmail.com>
2023-09-23 20:51:07 -0400

    Implement StartGame(), UpdateGame(), and EndGame()
    Currently, StartGame sets event listeners and resets scores,
    UpdateGame handles scores from each round, and EndGame displays
    the winner with 5 points and calls StartGame. Need to Implement
    score display on the page.

--- game.js
+++ game.js
@@ -1,24 +1,46 @@
-// Game();
-
-
 const scissorsButton = document.querySelector('#scissors');
 const rockButton = document.querySelector('#rock');
 const paperButton = document.querySelector('#paper');
+let playerScore = 0;
+let computerScore = 0;
 const scoreDisplay = document.querySelector('#score');
-AddEventHandlers();
+
+StartGame();
+
 function AddEventHandlers() {
     scissorsButton.addEventListener('click', () => SingleRound('Scissors'));
     rockButton.addEventListener('click', () => SingleRound('Rock'));
     paperButton.addEventListener('click', () => SingleRound('Paper'));
 }
 
-// function GetPlayerChoice() {
-//     let choice = 0;
-//     while (choice < 1 || choice > 3 || Number.isNaN(choice)) {
-//     choice = parseInt(prompt("Please choose!\n1: Rock\n2: Paper\n3: Scissors"));
-//     }
-//     return ParseChoice(choice);
-// }
+function StartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    AddEventHandlers();
+}
+function UpdateGame(winner) {
+    if (winner === "Tie") console.log("It's a tie!");
+    else if (winner === 1) {
+        console.log("You win!");
+        playerScore++;
+    } else if (winner === 2) {
+        console.log("Computer wins!");
+        computerScore++;
+    }
+    console.log(`The score is: You - ${playerScore}, Computer - ${computerScore}`);
+    if (playerScore === 5) {
+        EndGame(1);
+    } else if (computerScore === 5) {
+        EndGame(2);
+    }
+    
+}
+
+function EndGame(winner) {
+    if (winner === 1) console.log('You won!');
+    else if (winner === 2) console.log('Computer won!');
+    StartGame();
+}
 function GetComputerChoice() {
     return ParseChoice(Math.floor(Math.random() * 3) + 1);
 }
@@ -39,66 +61,8 @@ function ParseChoice(choice) {
 function SingleRound(playerChoice) {
     let computerChoice = GetComputerChoice();
     console.log(computerChoice);
-    // return DetermineRoundWinner(playerChoice, computerChoice);
-    // scoreDisplay.textContent = DetermineRoundWinner(playerChoice, computerChoice);
-    DetermineRoundWinner(playerChoice, computerChoice);
+    UpdateGame(DetermineRoundWinner(playerChoice, computerChoice));
 }
-
-// function Game() {
-//     let round;
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     // for (let i = 1; i <= 5; i++) {
-//         round = SingleRound();
-//         if (round === "Tie") console.log("It's a tie!");
-//         else if (round === 1) {
-//             console.log("You win!");
-//             playerScore++;
-//         } else if (round === 2) {
-//             console.log("Computer wins!");
-//             computerScore++;
-//         }
-//         console.log(`The score is: You - ${playerScore}, Computer - ${computerScore}`);
-//     // }
-//     if (playerScore > computerScore) {
-//         console.log("You win!");
-//     } else if (computerScore > playerScore) {
-//         console.log("Computer wins!");
-//     } else console.log("Not sure what happened! Tie?");
-// }
-
-// function DetermineRoundWinner(player1, player2) {
-//     if (player1 === player2) return "Tie";
-//     switch(player1) {
-//         case "Rock":
-//             if (player2 === "Paper") {
-//                 console.log("Paper beats rock!"); 
-//                 return 2;
-//             } else {
-//                 console.log("Rock beats Scissors!");
-//                 return 1;
-//             }
-//             break;
-//         case "Paper":
-//             if (player2 === "Scissors") {
-//                 console.log("Scissors beats Paper!");
-//                 return 2;
-//             } else {
-//                 console.log("Paper beats Rock!");
-//                 return 1;
-//             }
-//             break;
-//         case "Scissors":
-//             if (player2 === "Rock") {
-//                 console.log("Rock beats Scissors!");
-//                 return 2;
-//             } else {
-//                 console.log("Scissors beats Paper!");
-//                 return 1;
-//             }
-//             break;
-//     }
-// }
 
 function DetermineRoundWinner(player1, player2) {
     if (player1 === player2) return "Tie";
